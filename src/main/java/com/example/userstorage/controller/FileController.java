4b4abb9d330288d3f80b6d48b846259ec83d1aea
Deep Pimpale <deeppimpale67@gmail.com>
2023-03-04 13:29:44 +0530

    push

--- src/main/java/com/example/userstorage/controller/FileController.java
+++ src/main/java/com/example/userstorage/controller/FileController.java
@@ -18,33 +18,37 @@ import java.util.List;
 @RestController
 public class FileController {
 
-    
+    public void setS3Client(AmazonS3 s3Client) {
+        this.s3Client = s3Client;
+    }
     
    @Autowired
    private AmazonS3 s3Client;
    @Value("${aws.s3.bucketName}")
     private String bucketName;
 
-   @GetMapping("/searchFiles")
+    @GetMapping("/searchFiles")
     public List<String> searchFiles(@RequestParam String userName, @RequestParam String searchTerm) {
        List<String> fileList = new ArrayList<>();
        String prefix = userName + "/";
-       ListObjectsV2Request req = new ListObjectsV2Request().withBucketName("userstoragebucket").withPrefix(prefix).withMaxKeys(1000);
+       ListObjectsV2Request req = new ListObjectsV2Request().withBucketName(bucketName).withPrefix(prefix).withMaxKeys(1000);
        ListObjectsV2Result result;
-
+    
        do {
            result = s3Client.listObjectsV2(req);
-
-           for (S3ObjectSummary objectSummary : result.getObjectSummaries()) {
-               String key = objectSummary.getKey();
-               if (key.contains(searchTerm)) {
-                   fileList.add(key);
+    
+           if (result != null) {
+               for (S3ObjectSummary objectSummary : result.getObjectSummaries()) {
+                   String key = objectSummary.getKey();
+                   if (key.contains(searchTerm)) {
+                       fileList.add(key);
+                   }
                }
+    
+               req.setContinuationToken(result.getNextContinuationToken());
            }
-
-           req.setContinuationToken(result.getNextContinuationToken());
-       } while (result.isTruncated());
-
+       } while (result != null && result.isTruncated());
+    
        return fileList;
     }
 }
